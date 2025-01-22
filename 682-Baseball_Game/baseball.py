e03335b39dd969bd76760286a73fd08dfc7cae8f
asad24-dev <asadmajeed2005@gmail.com>
2025-01-22 21:57:08 +0000

    problem no. 682

---
+++ 682-Baseball_Game/baseball.py
@@ -0,0 +1,19 @@
+def calPoints(operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        ret = []
+        for i in range(len(operations)):
+            if operations[i] == 'D':
+                ret.append(ret[-1] * 2)
+            elif operations[i] == 'C':
+                ret.pop()
+            elif operations[i] == '+':
+                ret.append(ret[-1] + ret[-2])
+            else:
+                ret.append(int(operations[i]))
+        sum = 0
+        for i in ret:
+            sum += i
+        return sum
