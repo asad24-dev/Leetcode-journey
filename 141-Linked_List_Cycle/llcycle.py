251ec8a8f6ffa61b9dea06d21c705473f945e8ca
asad24-dev <asadmajeed2005@gmail.com>
2025-01-29 20:13:28 +0000

    comment

--- 141-Linked_List_Cycle/llcycle.py
+++ 141-Linked_List_Cycle/llcycle.py
@@ -11,3 +11,12 @@ def hasCycle(head):
             if slow == fast:
                 return True
         return False
+
+"""
+When there is no cycle in the list, the loop ends when the fast pointer becomes null. 
+If a cycle exists, the fast pointer moves faster and continuously loops through the cycle. 
+With each step, it reduces the gap between itself and the slow pointer by one node. 
+For example, if the gap is 10, the slow pointer moves by 1, increasing the gap to 11, 
+while the fast pointer moves by 2, reducing the gap to 9. This process continues until 
+the fast pointer catches up to the slow pointer, confirming a cycle.
+"""
